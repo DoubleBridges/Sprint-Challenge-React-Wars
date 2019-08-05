506f847dda1f154c276807a1d8d524fe9714673b
Dave Kane <davelkane@gmail.com>
2019-08-05 00:13:42 -0500

    Added home world

--- starwars/src/components/CharacterCards/CharacterCards.js
+++ starwars/src/components/CharacterCards/CharacterCards.js
@@ -11,7 +11,6 @@ const CardDisplay = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  /* align-items: center; */
   margin: 50px auto;
   
  `;
@@ -23,76 +22,70 @@ const PageButtonContainer = styled.span`
 
 const CharacterCards = () => {
 
-    const [personArr, setPersonArr] = useState([]);
-    const [prevPage, setPrevPage] = useState('');
-    const [nextPage, setNextPage] = useState('');
-    const [api, setApi] = useState(`https://swapi.co/api/people/`);
-
-    // const buttons = document.querySelectorAll('.button')
-
-    useEffect(() => {
-        axios.get(api)
-            .then(res => {
-                setPersonArr(res.data.results)
-                setPrevPage(res.data.previous)
-                setNextPage(res.data.next)
-                console.log(res)
-            })
-            .catch(err => console.log(err))
-    }, [api]);
-
-    const prevPageHandler = () => setApi(prevPage)
-    const nextPageHandler = () => setApi(nextPage)
+  const [personArr, setPersonArr] = useState([]);
+  const [prevPage, setPrevPage] = useState('');
+  const [nextPage, setNextPage] = useState('');
+  const [api, setApi] = useState(`https://swapi.co/api/people/`);
 
 
-    const cards = personArr.map(person => {
-
-        // setHomeworldLink(person.homeworld)
-
-        const stats = (
-            <ul>
-                <li>Height: {person.height}</li>
-                <li>Mass: {person.mass}</li>
-                <li>Hair Color: {person.hair_color}</li>
-                <li>Eye Color: {person.eye_color}</li>
-                <li>Gender: {person.gender}</li>
-            </ul>
-        )
-
-        const homeworld = (
-            <a href={person.homeworld}>Home Planet</a>
-        )
+  useEffect(() => {
+    axios.get(api)
+      .then(res => {
+        setPersonArr(res.data.results)
+        setPrevPage(res.data.previous)
+        setNextPage(res.data.next)
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+  }, [api]);
 
 
-        return (
-            <CharacterCard
+  const prevPageHandler = () => setApi(prevPage)
+  const nextPageHandler = () => setApi(nextPage)
 
-                key={person.url}
-                header={person.name}
-                description={stats}
-                meta={homeworld}></CharacterCard>
 
-        )
-    })
+  const cards = personArr.map(person => {
 
-    if (personArr === [])
-        return <div>Loading...</div>
+    const stats = (
+      <ul>
+        <li>Height: {person.height}</li>
+        <li>Mass: {person.mass}</li>
+        <li>Hair Color: {person.hair_color}</li>
+        <li>Eye Color: {person.eye_color}</li>
+        <li>Gender: {person.gender}</li>
+      </ul>
+    )
+
 
     return (
-        <CardDisplay>
-            <Card.Group
-                centered
-                itemsPerRow={2}>
-                {cards}
-            </Card.Group>
-            <PageButtonContainer>
-                <PageButtons
-                    prevPage={prevPageHandler}
-                    nextPage={nextPageHandler}
-                />
-            </PageButtonContainer>
-        </CardDisplay>
+      <CharacterCard
+        key={person.url}
+        header={person.name}
+        description={stats}
+        planet={person.homeworld}></CharacterCard>
     )
+  })
+
+
+
+  if (personArr === [])
+    return <div>Loading...</div>
+
+  return (
+    <CardDisplay>
+      <Card.Group
+        centered
+        itemsPerRow={2}>
+        {cards}
+      </Card.Group>
+      <PageButtonContainer>
+        <PageButtons
+          prevPage={prevPageHandler}
+          nextPage={nextPageHandler}
+        />
+      </PageButtonContainer>
+    </CardDisplay>
+  )
 }
 
 export default CharacterCards
--- starwars/src/components/CharcterCard/CharacterCard.js
+++ starwars/src/components/CharcterCard/CharacterCard.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Card } from 'semantic-ui-react'
 
 
 
 const CharacterCard = (props) => {
+
+    const [planet, setPlanet] = useState('');
+
+    const planetUrl = props.planet
+
+
+    useEffect(() => {
+        axios.get(planetUrl)
+            .then(res => {
+                setPlanet(res.data.name)
+            })
+    })
+
 
     return (
         <Card
@@ -11,7 +25,7 @@ const CharacterCard = (props) => {
             raised
             header={props.header}
             description={props.description}
-            meta={props.meta}></Card>
+            meta={`Home World: ${planet}`}></Card>
     )
 }
 
